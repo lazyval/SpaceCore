10610f1872a1de26e2f5e12fe639443b9396b8b5
Golikov Konstantine <JohnySilver7@gmail.com>
2012-06-20 22:27:22 +0400

    Some code clarification, scoping exception

--- src/main/scala/com/github/omnomnom/spacecore/OBJParser.scala
+++ src/main/scala/com/github/omnomnom/spacecore/OBJParser.scala
@@ -16,22 +16,25 @@ trait OBJParser {
   val RESOURCES = "src/main/resources/"
 
   def loadModel(path: String) {
+    val filename = RESOURCES + path
 
     try {
-      Source.fromFile(RESOURCES + path).getLines().foreach {
+      Source.fromFile(filename).getLines().foreach {
         line =>
           line.split(' ').toList match {
             case "v" :: rest =>
               val List(x, y, z) = rest.map(_.toFloat)
+              // v defined in container: Model
               v += new Vector3f(x, y, z)
             case "f" :: rest =>
               val List(x, y, z) = rest.map(_.toFloat)
+              // f defined in container: Model
               f += new Face(new Vector3f(x, y, z))
             case x => log.error("Unknown token" + x)
           }
       }
     } catch {
-      case e: Exception => log.error(e)
+      case e: java.io.IOException => log.error("Problems with file %s IO".format(filename))
     }
     log.debug("Model " + path + " parsed succsessfully")
   }
